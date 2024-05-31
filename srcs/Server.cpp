c2558c1c9c01a46a3e33ce860aba2eadce4b4d45
Claudio Raimondi <craimond@c2r4p9.42firenze.it>
2024-05-31 17:18:31 +0200

    Add TODO

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 00:23:51 by craimond          #+#    #+#             */
-/*   Updated: 2024/05/31 17:17:08 by craimond         ###   ########.fr       */
+/*   Updated: 2024/05/31 17:18:12 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -293,7 +293,7 @@ void Server::handleClient(const int client_socket)
 		_handler.setClient(*client);
 		_handler.processInput(raw_input);
 	}
-	catch (const ProtocolErrorException &e)
+	catch (const ProtocolErrorException &e) //TODO vlautare se catchare le SystemErrorException qui, dato che il server non deve mai crashare
 	{
 		struct s_message msg = e.getContent();
 		msg.params.insert(msg.params.begin(), client->getNickname());
